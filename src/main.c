ca25ee06f8ecdf4e537653e3dddb85ed9a288937
Andrew Young <andrewyoung@Iains-MBP.home>
2021-06-08 10:02:35 +0100

    added utils

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("FORMULATE 1.0.0\n");
+int main(int argc, char** argv) {
+    printf("FORMULATE 1.0.0\n\n");
+
+    if (argc < 3) {
+        printf("Too few arguments\n");
+    }
 
     return 0;
 }
---
+++ src/util.c
@@ -0,0 +1,5 @@
+#include "../inc/util.h"
+
+char* readAsciiFile(const char* path) {
+    
+}
